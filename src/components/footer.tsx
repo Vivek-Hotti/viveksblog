86b2cfcf14838d8ef373a5895319db69bf012d73
Vivek Hotti <vivekshotti@gmail.com>
2022-01-08 19:08:28 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,11 +6,23 @@ export default function Footer() {
       <footer>
         <span>
           <ExtLink href="https://www.linkedin.com/in/vivekhotti/">
-            LinkedIN
+            LinkedIn
           </ExtLink>
           {' | '}
           <ExtLink href="https://github.com/Vivek-Hotti">
             GitHub
+          </ExtLink>
+          {' | '}
+          <ExtLink href="mailto:vivekshotti@gmail.com">
+            Mail
+          </ExtLink>
+          {' | '}
+          <ExtLink href="https://www.facebook.com/vivek.hotti.9/">
+            Facebook
+          </ExtLink>
+         {' | '}
+          <ExtLink href="https://www.instagram.com/vivek_hotti/">
+            Instagram
           </ExtLink>
         </span>
       </footer>
